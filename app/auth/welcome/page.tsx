405f03af315b0c18e1f28220e21d23a2c6d82e76
Matthias <matthias.mulkens@hotmail.com>
2025-11-23 17:44:34 +0100

    Viewport heigt for iphone.

--- app/auth/welcome/page.tsx
+++ app/auth/welcome/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { createClient } from "@/utils/supabase/server";
 
 export default function HomePage() {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,17 @@ import { createClient } from "@/utils/supabase/server";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen max-v-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Trip Planner</h1>
-      
+    <main>
+      <h1>Welcome to the Surprise Communications Dashboard</h1>
+      <p className="text-lg">Please log in to access your dashboard and plan your next trip!</p>
+      <div className="mt-4 justify-center flex gap-4">
+        <Link href="/auth/login">
+          <button className="bg-red-600 hover:bg-red-500">Log In</button> 
+        </Link>
+        <Link href="/auth/signup">
+          <button className="bg-gray-400 hover:bg-gray-500">Sign Up</button>
+        </Link>
+      </div>
     </main>
   )
 }
