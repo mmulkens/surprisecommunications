4d529a2af23082cf4b9c28f68579334f7d61a5c8
Matthias <matthias.mulkens@hotmail.com>
2025-11-28 22:07:00 +0100

    Minor enhancements

--- app/auth/welcome/page.tsx
+++ app/auth/welcome/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main>
-      <p className="bg-white/90 px-5 py-8 rounded-full text-6xl">🏕️</p>
+      <div className="bg-white/90 px-5 py-8 rounded-full text-6xl">🏕️</div>
       <h3 className="text-2xl my-6">WELCOME TO THE</h3>
-      <h1>Surprise Communications Dashboard</h1>
+      <h1 className="text-xl">Surprise Communications Dashboard</h1>
       <p className="text-lg">Please log in to access your dashboard and plan your next trip!</p>
       <div className="mt-4 justify-center flex gap-4">
         <Link href="/auth/login">
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import userDataFetch from "./getUser";
 import  userChanceFetch from "./getChance";
 import  userChancesFetch from "./getChances";
+import { getDefaultAutoSelectFamily } from "net";
 
 export default async function DashboardPage() {
   const { user, profile, nameToShow } = await userDataFetch();
@@ -15,12 +16,13 @@ export default async function DashboardPage() {
         <>
           <p className="text-8xl mb-6">🚀</p>
           <h1>Welcome, {nameToShow}</h1>
-          <p>Your role for the next trip will appear here.</p>
           <p>The chance you'll be the next organizer is</p>
-          <p className="text-7xl bg-white/30 w-72 rounded-lg pt-3 pb-4 px-6 text-center text-white font-bold my-6">{drawChance}%</p>
+          <div className="text-7xl bg-white/30 w-72 mt-6 rounded-lg pt-3 pb-4 px-6 text-center text-white font-bold">{drawChance}%</div>
           
-          <p className="text-8xl">🪂</p>
-          <div className="mt-6 w-72 bg-white/30 p-4 rounded-lg">
+          <div className="text-8xl my-6">🪁</div>
+          
+          <p>Other participants' chances are</p>
+          <div className="w-72 mt-6 bg-white/30 p-4 rounded-lg">
             {drawChances.map((item, index) => (
               <div className="flex text-sm mt-1.5 mb-1.5">
                 <span key="item.user_id" className="w-14 pr-2 text-xs text-white text-right">{item.users.voornaam}</span>
@@ -37,10 +39,13 @@ export default async function DashboardPage() {
             ))}
           </div>
 
+          <div className="text-8xl my-6">🎲</div>
+          <p>Your role for the next trip will appear here.</p>
           
+          <div className="text-6xl bg-white/30 w-72 mt-6 rounded-lg pt-3 pb-4 px-6 text-center text-white font-bold">Traveler</div>
           
           <form action="/auth/logout" method="post">
-            <div className="flex justify-between mt-6">
+            <div className="flex justify-between mt-10">
               <button type="submit" className="pt-4 bg-red-500 hover:bg-red-600">Log out</button>
             </div>
           </form>
