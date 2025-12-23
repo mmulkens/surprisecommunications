bfb4ee2f7298aded0e00794c732db9168a580aa5
Matthias <matthias.mulkens@hotmail.com>
2025-12-23 10:38:15 +0100

    Logout route change?

--- app/auth/logout/route.ts
+++ app/auth/logout/route.ts
@@ -1,10 +1,11 @@
+"use server";
 import { createClient } from "@/utils/supabase/server";
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
 
 export async function POST(req: Request) {
   const supabase = await createClient();
-
   await supabase.auth.signOut();
-
-  return NextResponse.redirect(new URL("/auth/welcome", req.url));
+  redirect("/auth/welcome");
+  // return NextResponse.redirect(new URL("/auth/welcome", req.url));
 }
